765e38b7ec6da5853e9a8c5b1d5dc6499a666e19
teodorstrom <teodorstrom.ts@gmail.com>
2022-06-01 19:49:02 +0000

    make a blog post clickable to view the full post

--- codestar2021/settings.py
+++ codestar2021/settings.py
@@ -20,6 +20,7 @@ if os.path.isfile("env.py"):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
@@ -63,7 +65,7 @@ ROOT_URLCONF = 'codestar2021.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
